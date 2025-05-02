efd23b5e98a8485b919019cd592c967a7b034796
OctavioSzMz17 <octavioSM@protonmail.com>
2025-05-01 19:03:54 -0600

    update connDB Available for ExternalDB

--- DBDriver/connDB.go
+++ DBDriver/connDB.go
@@ -1,26 +1,42 @@
 package DBDriver
 
 import (
-	"database/sql"
-	"log"
-
-	_ "github.com/go-sql-driver/mysql"
+    "database/sql"
+    "log"
+    _ "github.com/go-sql-driver/mysql"
+    _ "github.com/lib/pq"
 )
 
+// DB es la variable global de conexión
 var DB *sql.DB
 
-// Inicializa la conexión a la base de datos
-func InitDB(GBD, user, pass, host, port, dbname string) {
-	// Conexión a la base de datos MySQL
-	var err error
-	DB, err = sql.Open(GBD, user+":"+pass+"@tcp("+host+":"+port+")/"+dbname)
-	if err != nil {
-		log.Fatal("Error al conectar con la base de datos:", err)
-	}
+// Inicializa la conexión a la base de datos MySQL
+func InitDBMYSQL(GBD, user, pass, host, port, dbname string) {
+    var err error
+    DSN := user + ":" + pass + "@tcp(" + host + ":" + port + ")/" + dbname
+    DB, err = sql.Open(GBD, DSN)
+    if err != nil {
+        log.Fatal("Error al conectar con la base de datos:", err)
+    }
 
-	// Verificar si la conexión es exitosa
-	err = DB.Ping()
-	if err != nil {
-		log.Fatal("No se puede establecer la conexión con la base de datos:", err)
-	}
+    // Verificar si la conexión es exitosa
+    err = DB.Ping()
+    if err != nil {
+        log.Fatal("No se puede establecer la conexión con la base de datos:", err)
+    }
+}
+
+// Inicializa la conexión a la base de datos externa (Supabase)
+func InitDBSupabase(connStr string) {
+    var err error
+    DB, err = sql.Open("postgres", connStr)
+    if err != nil {
+        log.Fatal("Error al conectar con la base de datos:", err)
+    }
+
+    // Verificar si la conexión es exitosa
+    err = DB.Ping()
+    if err != nil {
+        log.Fatal("No se puede establecer la conexión con la base de datos:", err)
+    }
 }
--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.24.1
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/go-sql-driver/mysql v1.9.2 // indirect
+	github.com/lib/pq v1.10.9 // indirect
 	golang.org/x/crypto v0.37.0 // indirect
 )
